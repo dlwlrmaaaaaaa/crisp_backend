60ba32a4039af755d586c4150e0a8c0c831ab0c9
Justin Marucut <143764790+dlwlrmaaaaaaa@users.noreply.github.com>
2024-09-23 08:15:32 +0800

    authentication

--- crisp_backend/crisp_backend/firebase.py
+++ crisp_backend/crisp_backend/firebase.py
@@ -1,8 +1,8 @@
-# import firebase_admin
-# from firebase_admin import credentials
+import firebase_admin
+from firebase_admin import credentials
 
-# cred = credentials.Certificate('C:/Users/codin/OneDrive/crisp-63736-firebase-adminsdk-r1i8j-a043e2e3ad.json')
+cred = credentials.Certificate('C:/Users/codin/OneDrive/crisp-63736-firebase-adminsdk-r1i8j-a043e2e3ad.json')
 
-# firebase_admin.initialize_app(cred, {
-#     'databaseURL': 'https://crisp-63736-default-rtdb.asia-southeast1.firebasedatabase.app/'
-# })
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://crisp-63736-default-rtdb.asia-southeast1.fSirebasedatabase.app/'
+})
